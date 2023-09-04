d18c1d8d4ef16d0de81d228d123fd58228f6db36
Marcio Curvello <marciocurvello@gmail.com>
2023-09-04 17:28:24 -0300

    Building a list we can delete from

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Marcio Curvello on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
